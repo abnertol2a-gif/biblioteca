2ffa75ab0e2457830cfa6e79a6c0f7e65cb3f2e8
abnertol2a-gif <abnertol2a@gmail.com>
2025-09-19 22:53:47 -0600

    Creating libro class.

---
+++ SistemaBiblioteca/SistemaBiblioteca/Libro.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SistemaBiblioteca
+{
+    public class Libro
+    {
+        public string Titulo { get; set; }
+        public string Isbn { get; set; }
+        public int AnioPublicacion { get; set; }
+        public Autor Autor { get; set; }  // Composición
+
+        public Libro(string titulo, string isbn, int anioPublicacion, Autor autor)
+        {
+            Titulo = titulo;
+            Isbn = isbn;
+            AnioPublicacion = anioPublicacion;
+            Autor = autor;
+        }
+
+        public void MostrarInformacion()
+        {
+            Console.WriteLine($"Título: {Titulo}");
+            Console.WriteLine($"ISBN: {Isbn}");
+            Console.WriteLine($"Año: {AnioPublicacion}");
+            Console.WriteLine($"Autor: {Autor}");
+            Console.WriteLine("-----------------------------------");
+        }
+
+        public override string ToString()
+        {
+            return $"{Titulo} ({AnioPublicacion}) - {Autor.Nombre}";
+        }
+    }
+}
