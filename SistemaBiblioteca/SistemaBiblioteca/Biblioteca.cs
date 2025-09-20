1b07093dc78aed840ef8a29db9c10a92d16ea035
abnertol2a-gif <abnertol2a@gmail.com>
2025-09-19 23:01:50 -0600

    Adding biblioteca class, which implements manage methods for library

---
+++ SistemaBiblioteca/SistemaBiblioteca/Biblioteca.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SistemaBiblioteca
+{
+    public class Biblioteca
+    {
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        private List<Libro> libros = new List<Libro>();
+
+        public Biblioteca(string nombre, string direccion)
+        {
+            Nombre = nombre;
+            Direccion = direccion;
+        }
+
+        public void AgregarLibro(Libro libro)
+        {
+            libros.Add(libro);
+        }
+
+        public void BuscarPorAutor(string nombreAutor)
+        {
+            var encontrados = libros.Where(l => l.Autor.Nombre.Equals(nombreAutor, StringComparison.OrdinalIgnoreCase)).ToList();
+
+            if (encontrados.Count == 0)
+            {
+                Console.WriteLine($"No se encontraron libros del autor {nombreAutor}.");
+            }
+            else
+            {
+                Console.WriteLine($"Libros de {nombreAutor}:");
+                foreach (var libro in encontrados)
+                {
+                    libro.MostrarInformacion();
+                }
+            }
+        }
+
+        public void ListarLibros()
+        {
+            Console.WriteLine("=== Libros en la biblioteca ===");
+            foreach (var libro in libros)
+            {
+                libro.MostrarInformacion();
+            }
+        }
+    }
+}
