01e9986a73bc10ade0d0481cde52b41e14fd225c
abnertol2a-gif <abnertol2a@gmail.com>
2025-09-19 22:47:57 -0600

    Creating main class Autor

---
+++ SistemaBiblioteca/SistemaBiblioteca/Autor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SistemaBiblioteca
+{
+    public class Autor
+    {
+        public string Nombre { get; set; }
+        public string Nacionalidad { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+
+        public Autor(string nombre, string nacionalidad, DateTime fechaNacimiento)
+        {
+            Nombre = nombre;
+            Nacionalidad = nacionalidad;
+            FechaNacimiento = fechaNacimiento;
+        }
+
+        public int GetEdad()
+        {
+            int edad = DateTime.Now.Year - FechaNacimiento.Year;
+            if (DateTime.Now.DayOfYear < FechaNacimiento.DayOfYear)
+                edad--;
+            return edad;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nombre} ({Nacionalidad}), {GetEdad()} años";
+        }
+    }
+}
